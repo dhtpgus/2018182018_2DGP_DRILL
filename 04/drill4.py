e05174fb14d75edb5e46b496ec6c324d9b6146f9
dhtpgus <dhtpgus1414@gmail.com>
2022-09-26 21:42:27 +0900

    drill 07
    2018182018 오세현

---
+++ 04/drill4.py
@@ -0,0 +1,37 @@
+import turtle
+
+count = 4
+while(count > 0):
+    turtle.forward(500)
+    turtle.left(90)
+    count -= 1
+
+
+count = 2
+while ( count > 0 ):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(500)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(500)
+    turtle.left(90)
+    count -= 1
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+count = 2
+while (count > 0):
+    turtle.forward(500)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(500)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    count -= 1
---
+++ 07/anim_viewer.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import os
+
+open_canvas()
+os.chdir('d:/2018182018_2DGP_DRILL/07')
+
+grass = load_image('grass.png')
+character = load_image('skulrun.png')
+
+x = 0
+frame = 0
+while x < 800:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 62, 0, 55, 100, x, 90, 90, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    x += 5
+    delay(0.05)
+    get_events()
+
+close_canvas()
