0a80982b99eae67a3409ebf3c1be4160cf002d1a
dhtpgus <dhtpgus1414@gmail.com>
2022-11-03 15:07:22 +0900

    lec 12

---
+++ lec/Lecture12_Game_World/game_world.py
@@ -0,0 +1,33 @@
+world = [[], []]  # 게임 월드는 객체들의 집합
+
+
+def add_object(o, depth):
+    world[depth].append(o)
+
+
+def add_objects(ol, depth):
+    world[depth] += ol
+
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            del o
+            return
+    raise ValueError('Trying destroy non existing object')
+    # world.remove(o) # 리스트에서만 삭제됨
+    # del o # 실제로 메모리에서까지 삭제
+
+
+def all_objects():
+    for layer in world:
+        for o in layer:
+            yield o
+
+
+def clear():
+    for o in all_objects():
+        del o
+    for layer in world:
+        layer.clear()
